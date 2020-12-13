ff82b3af4dfa79d6ded25ccc1273d2d052ab5b51
Jason Kulatunga <jason@thesparktree.com>
2020-12-13 13:37:42 -0800

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# forklift
+
+ SystemD service to deploy always up-to-date kernel modules for Flatcar Container Linux
+
+# Background
+
+Compiling drivers on Flatcar Container Linux is typically non-trivial because the OS ships without build tools and no
+obvious way to access the kernel sources. Forklift works by pre-compiling your kernel modules inside of a Flatcar Container Linux developer
+container. On startup the Forklift service will pull down the docker image containing pre-compiled kernel module binaries that
+match your Flatcar version, cache the binaries on your host, and then load the kernel modules.
+
+This project currently supports generating NVIDIA kernel modules, however it can be extended to support any module.
+
+Forklift is based on the following CoreOS projects:
+
+- https://github.com/squat/modulus
+- https://github.com/BugRoger/coreos-nvidia-driver
+
+# SystemD
+
+```
+sudo git clone https://github.com/mediadepot/flatcar-forklift.git /opt/flatcar-forklift
+sudo cp /opt/flatcar-forklift/forklift@.service /etc/systemd/system/forklift@.service
+
+sudo systemctl enable forklift@nvidia
+sudo systemctl start forklift@nvidia
+```
---
+++ forklift.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ]
+  then
+    echo "No driver name supplied"
+    exit -1
+fi
+
+# Prepare Environment
+export FORKLIFT_DRIVER_NAME=${0}
+export FORKLIFT_INSTALL_DIR=${FORKLIFT_INSTALL_DIR:-/opt/drivers}
+export FORKLIFT_CACHE_DIR="${FORKLIFT_INSTALL_DIR}/archive/${FORKLIFT_DRIVER_NAME}"
+
+# Prepare Filesystem
+mkdir -p "$FORKLIFT_INSTALL_DIR"
+mkdir -p "$FORKLIFT_CACHE_DIR"
+rm -rf "${FORKLIFT_INSTALL_DIR:?}/${FORKLIFT_DRIVER_NAME}"
+
+# Extract Drivers From Docker Image
+if [ -d "${FORKLIFT_CACHE_DIR}/${FLATCAR_RELEASE_VERSION}" ]
+then
+    echo "Drivers for ${FORKLIFT_DRIVER_NAME}@${FLATCAR_RELEASE_VERSION} already exist locally"
+else
+    docker pull mediadepot/flatcar-${FORKLIFT_DRIVER_NAME}-driver:flatcar_${FLATCAR_RELEASE_VERSION}-${FORKLIFT_DRIVER_NAME}_latest
+    docker run --rm -v ${FORKLIFT_CACHE_DIR}/${FLATCAR_RELEASE_VERSION}:/out mediadepot/flatcar-${FORKLIFT_DRIVER_NAME}-driver:flatcar_${FLATCAR_RELEASE_VERSION}-${FORKLIFT_DRIVER_NAME}_latest
+fi
+
+# setup symlink from cache directory to "install" directory
+ln -s "${FORKLIFT_CACHE_DIR}/${FLATCAR_RELEASE_VERSION}" "${FORKLIFT_INSTALL_DIR}/${FORKLIFT_DRIVER_NAME}"
+
+if [ -d "${FORKLIFT_INSTALL_DIR}/${FORKLIFT_DRIVER_NAME}/lib" ] ; then
+    mkdir -p "${FORKLIFT_LD_ROOT}/etc/ld.so.conf.d"
+    echo "${FORKLIFT_INSTALL_DIR}/${FORKLIFT_DRIVER_NAME}/lib" > "${FORKLIFT_LD_ROOT}/etc/ld.so.conf.d/${FORKLIFT_DRIVER_NAME}.conf"
+    ldconfig -r "${FORKLIFT_LD_ROOT}" 2> /dev/null
+fi
+
+# shellcheck disable=SC1090
+source "${FORKLIFT_INSTALL_DIR}/${FORKLIFT_DRIVER_NAME}/install.sh"
+
