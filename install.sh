08ab02e035387cbffff292de04e314f45d65f5cd
Jason Kulatunga <jason@thesparktree.com>
2022-07-22 18:51:49 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 mkdir /opt/overlay/modules
 mkdir /opt/overlay/modules.wd
 
-cp ./usr-lib64-modules.mount /etc/systemd/system/usr-lib64-modules.mount
+cp ./usr-lib-modules.mount /etc/systemd/system/usr-lib-modules.mount
 
 # install flatcar-forklift service
 cp /opt/flatcar-forklift/forklift@.service /etc/systemd/system/forklift@.service
